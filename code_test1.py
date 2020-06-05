dfb2008ccf8208ae6e2a27d031b86e5a54f1e6fc
Shady311 <66484760+Shady311@users.noreply.github.com>
2020-06-06 02:16:57 +0300

    Update code_test1.py

--- code_test1.py
+++ code_test1.py
@@ -1,17 +1,17 @@
 with open('dataset_3363_2.txt') as inf:
     cod = str(inf.readline())
     dnk = ('')
-    i = 1
     n = cod[0]
-    v = ('')
-    for i in range(1, len(cod)):
-        k = cod[i]
-        if k.isdigit():
-            v = v + k
-        else:
-            dnk = dnk + n * int(v)
+    z = ('')
+    for i in range(len(cod)):
+        if not cod[i].isdigit():
             n = cod[i]
-            v = ('')
-    dnk = dnk + n * int(v)
+        elif i + 1 < len(cod) and not cod[i+1].isdigit():
+            z += cod[i]
+            z = int(z)
+            dnk = dnk + n * z
+            z = ('')
+        else:
+            z += cod[i]
 with open('output.txt', 'w') as ouf:
     ouf.write(dnk)
